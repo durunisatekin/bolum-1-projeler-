235cbf037fb60980f04967c8c4131d2cac5d6bed
Duru Tekin <duru.tekin@ekofer.int>
2026-01-19 14:08:04 +0300

    ilk-temiz-yukleme

---
+++ veritipleri_degiskenler.dart
@@ -0,0 +1,51 @@
+
+/* String=metinsel ifadeler 
+ Double =ondalıklı sayılar 
+Boolean= true-false 
+ int=tam sayılar 
+normalde int bir tam sayıdır ve null olmaz sonuna ? ekleni değişken hem bir int değeri tutabilir hem de null olabilir.
+ bu değişkenlerin yerinde otomatik olarak " var" kullanabilir. }
+ ${ad}= sadece değişken
+ ${ad.toUpperCase()}= değişkene işlem/metot uygulayıp ekleme 
+ ${5+3}= hesap yapıp ekleme
+${user.name}=iç içe değere erişme
+*/ 
+ void main(List<String> args) {
+   int yas = 19 ;
+   num ortalama = 4  ;
+   print(yas);
+   print(ortalama);
+
+   String ad ='duru' ;
+   print(ad) ;
+   print(ad + 'nun en sevdiği renkler') ;
+   print(ad + 'nun yaşı ') ;
+  
+ }
+ 
+ 
+
+  
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
